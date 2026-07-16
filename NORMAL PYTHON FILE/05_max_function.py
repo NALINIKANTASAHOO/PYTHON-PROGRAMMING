d24bad66592b35a10149e0780407402f7f1988b2
NALINI KANTA SAHOO <nalini20121136@gmail.com>
2026-07-16 19:38:59 +0530

    Add files via upload

---
+++ NORMAL PYTHON FILE/05_max_function.py
@@ -0,0 +1,7 @@
+# Concept: max() Function
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("The maximum number is:", max(a, b, c))
+print("The minimum number is:", min(a, b, c))
---
+++ NORMAL PYTHON FILE/06_for_loop.py
@@ -0,0 +1,6 @@
+# Concept: for Loop
+n = int(input("Enter a number (to print 1 to n): "))
+
+for i in range(1, n + 1):
+    print(i, end=" ")
+print()
